cd8f973b43e08029fa12efdbe16fbdf1d506dcc2
Prometheus <167355459+prometheus0717@users.noreply.github.com>
2025-10-16 15:00:21 +0530

    Update user.model.js

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -4,7 +4,7 @@ const userScheme = new Schema(
     {
         name: { type: String, required: true },
         username: { type: String, required: true, unique: true },
-        password: { type: String, required: true },
+        password: { type: String, required: true }, 
         token: { type: String }
     } 
 )
