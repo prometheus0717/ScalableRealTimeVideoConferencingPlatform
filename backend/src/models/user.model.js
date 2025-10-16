f5e150397f9ecc250ef62670e2d40fbc65ec953a
Prometheus <167355459+prometheus0717@users.noreply.github.com>
2025-10-16 15:00:32 +0530

    Update user.model.js

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,7 +1,7 @@
 import mongoose, { Schema } from "mongoose";
 
 const userScheme = new Schema(
-    {
+    { 
         name: { type: String, required: true },
         username: { type: String, required: true, unique: true },
         password: { type: String, required: true }, 
